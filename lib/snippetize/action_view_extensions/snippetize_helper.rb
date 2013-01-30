dbd46092964f4c7ba3333eb78a10d033359b4173
Mathieu Gagné <mathieu@motioneleven.com>
2013-01-30 16:50:57 -0500

    Handles empty string

--- lib/snippetize/action_view_extensions/snippetize_helper.rb
+++ lib/snippetize/action_view_extensions/snippetize_helper.rb
@@ -2,7 +2,7 @@ module Snippetize
   module ActionViewExtensions
     module SnippetizeHelper
 
-      def snippetize string
+      def snippetize string=''
         string.gsub(/\{(.*?)\}/) do |s|
           snippet = Snippetize::Snippet.new($1)
           render partial: snippet.path, locals: snippet.locals
