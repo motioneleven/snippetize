f5b0a95b502c98151b90a6a4773d663e4b4804da
Mathieu Gagné <mathieu@motioneleven.com>
2013-03-07 09:32:06 -0500

    Default delimiter changed to double brackets {{}}

--- lib/snippetize/action_view_extensions/snippetize_helper.rb
+++ lib/snippetize/action_view_extensions/snippetize_helper.rb
@@ -2,8 +2,10 @@ module Snippetize
   module ActionViewExtensions
     module SnippetizeHelper
 
+      # Renders the first expression within double brackets {{}} as a partial
+      # Other variables are passed as parameters to the partial
       def snippetize string=''
-        string.gsub(/\{(.*?)\}/) do |s|
+        string.gsub(/\{\{(.*?)\}\}/) do |s|
           snippet = Snippetize::Snippet.new($1)
           render partial: snippet.path, locals: snippet.locals
         end
