dfe71a68ed91c73524e58a04a4965b170eea915a
Alexandre Croteau-Pothier <alex@motioneleven.com>
2013-07-04 15:28:14 -0400

    rename the argument to be more clear

--- lib/snippetize/snippet.rb
+++ lib/snippetize/snippet.rb
@@ -3,8 +3,8 @@ module Snippetize
 
     attr_accessor :partial, :path, :locals
 
-    def initialize(snippet)
-      @partial, params = snippet.split(/[\s]/, 2)
+    def initialize(snippet_value)
+      @partial, params = snippet_value.split(/[\s]/, 2)
       @locals = Rack::Utils.parse_nested_query(params).try(:symbolize_keys!)
       @path = "#{Snippetize.location}/#{@partial}"
     end
